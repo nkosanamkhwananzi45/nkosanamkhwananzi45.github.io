a191e5a7b5361af70f1e610a778ea5454fbbcb98
nativeberry <nkosanamkhwananzi45@gmail.com>
2026-04-02 05:16:26 +0200

    Update vite.config.ts
    Signed-off-by: nativeberry <nkosanamkhwananzi45@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
+    base: "/",
     port: 8080,
     hmr: {
       overlay: false,
